e1a542bfff07c7786b09989ac77d5ae008df55e7
John Comeau <jc@unternet.net>
2024-05-04 16:52:01 +0000

    no help using tk.Label vs ttk

--- stopgap/xtest.py
+++ stopgap/xtest.py
@@ -3,14 +3,15 @@
 test that X is running and will display our windows
 '''
 import logging
-from tkinter import Tk, ttk
+import tkinter as tk
+from tkinter import ttk
 logging.basicConfig(level=logging.DEBUG if __debug__ else logging.WARNING)
 logging.debug('starting program')
-window = Tk()
+window = tk.Tk()
 logging.debug('window launched')
 window.title('X test')
 logging.debug('title set')
-label = ttk.Label(window, text='X is running')
+label = tk.Label(window, text='X is running')
 logging.debug('label created')
 label.pack()
 logging.debug('label packed, starting mainloop')
