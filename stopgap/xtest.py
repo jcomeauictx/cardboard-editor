b7b4bec8bd3f15c7dbe6503bf8931957d7ecad3c
John Comeau <jc@unternet.net>
2024-05-04 15:22:22 +0000

    segfaults after 'title set'

--- stopgap/xtest.py
+++ stopgap/xtest.py
@@ -2,9 +2,16 @@
 '''
 test that X is running and will display our windows
 '''
+import logging
 from tkinter import Tk, ttk
+logging.basicConfig(level=logging.DEBUG if __debug__ else logging.WARNING)
+logging.debug('starting program')
 window = Tk()
+logging.debug('window launched')
 window.title('X test')
+logging.debug('title set')
 label = ttk.Label(window, text='X is running')
+logging.debug('label created')
 label.pack()
+logging.debug('label packed, starting mainloop')
 window.mainloop()
