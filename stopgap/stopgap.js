b164565e618f29d3bf74ff39fd34e2eca2807a1b
John Comeau <jc@unternet.net>
2024-07-02 19:03:44 +0000

    no more keyboard popup but *still* window size doubles

--- stopgap/stopgap.js
+++ stopgap/stopgap.js
@@ -1,6 +1,7 @@
 document.onload = function() {
     var editwindow = document.getElementById("edit-window")
-    editwindow.onclick = function(event) {
-        return false;
-    }
+    var disregard = function(event) {return false};
+    ["click", "focus", "touchstart"].forEach(function(eventname) {
+        editwindow.addEventListener(eventname, disregard, false);
+    });
 }
