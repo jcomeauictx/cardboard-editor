fe2e8f5364d8b32febf14866184f980a17252fd6
John Comeau <jc@unternet.net>
2024-07-06 07:29:21 -0700

    working on standards-mode, not as good as quirks mode yet

--- stopgap/stopgap.js
+++ stopgap/stopgap.js
@@ -1,9 +1,12 @@
-document.onload = function() {
-    alert("Compatibility mode: " + document.compatMode);
+document.addEventListener("load", function() {
+    console.log("Compatibility mode: " + document.compatMode);
     var editwindow = document.getElementById("edit-window")
     var disregard = function(event) {return false};
-    var eventlist = ["click", "mousedown", "mouseup", "focus", "focusin", "touchstart"];
+    var eventlist = [
+        "click", "mousedown", "mouseup", "focus", "focusin", "touchstart"
+    ];
     eventlist.forEach(function(eventname) {
         editwindow.addEventListener(eventname, disregard, false);
     });
-};
+}, false);
+console.log("stopgap.js loaded");
