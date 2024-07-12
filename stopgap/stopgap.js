5883142bc32272d8fb952bc776a5d99399048ac9
John Comeau <jc@unternet.net>
2024-07-11 19:05:32 -0700

    got rid of syntax error

--- stopgap/stopgap.js
+++ stopgap/stopgap.js
@@ -1,8 +1,8 @@
 window.addEventListener("load", function() {
     const editWindow = document.getElementById("edit-window");
     const caretPosition = {
-        start: editWindow.selectionStart;
-        end: editWindow.selectionEnd;
+        start: editWindow.selectionStart,
+        end: editWindow.selectionEnd
     };
     editWindow.addEventListener("focusout", function() {
         caretPosition.start = editWindow.selectionStart;
