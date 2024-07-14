54a64ed548085a281a61a03623c5b76f04e06f8c
John Comeau <jc@unternet.net>
2024-07-14 06:32:45 -0700

    better but fake caret no longer shows unless hovering in inspector

--- stopgap/stopgap.js
+++ stopgap/stopgap.js
@@ -1,5 +1,11 @@
 window.addEventListener("load", function() {
     const editWindow = document.getElementById("edit-window");
+    const background = document.getElementById("background");
+    const fakeCaret = document.getElementById("fake-caret");
+    let styles = ["padding", "borderWidth", "borderStyle", "margin"];
+    styles.forEach(function(style) {
+        background.style[style] = getComputedStyle(editWindow)[style];
+    });
     const caretPosition = {
         start: editWindow.selectionStart,
         end: editWindow.selectionEnd
@@ -7,6 +13,8 @@ window.addEventListener("load", function() {
     editWindow.addEventListener("focusout", function() {
         caretPosition.start = editWindow.selectionStart;
         caretPosition.end = editWindow.selectionEnd;
+        console.debug("caretPosition: ", caretPosition);
     });
 }, false);
 console.log("stopgap.js loaded");
+// vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
