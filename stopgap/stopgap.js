4fd9cc1244c79cd3f5acb43a0fb2bba7e675784e
John Comeau <jc@unternet.net>
2024-07-14 07:15:46 -0700

    almost to master, and fake-caret still shows

--- stopgap/stopgap.js
+++ stopgap/stopgap.js
@@ -1,5 +1,8 @@
 window.addEventListener("load", function() {
     const editWindow = document.getElementById("edit-window");
+    const background = document.getElementById("background");
+    const fakeCaret = document.getElementById("fake-caret");
+    let styles = ["padding", "borderWidth", "borderStyle", "margin"];
     const caretPosition = {
         start: editWindow.selectionStart,
         end: editWindow.selectionEnd
