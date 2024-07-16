bae0d6081769f1c23ca9a6a06cc5b62862fc1946
John Comeau <jc@unternet.net>
2024-07-15 21:50:46 -0700

    replaceChildren() doesn't work as I thought it does

--- stopgap/stopgap.js
+++ stopgap/stopgap.js
@@ -2,6 +2,7 @@ window.addEventListener("load", function() {
     const editWindow = document.getElementById("edit-window");
     const background = document.getElementById("background");
     const fakeCaret = document.getElementById("fake-caret");
+    fakeCaret.parentNode.removeChild(fakeCaret);  // remove from DOM
     let styles = ["padding", "borderWidth", "borderStyle", "margin"];
     styles.forEach(function(style) {
         background.style[style] = getComputedStyle(editWindow)[style];
@@ -11,9 +12,15 @@ window.addEventListener("load", function() {
         end: editWindow.selectionEnd
     };
     editWindow.addEventListener("focusout", function() {
+        let editText = editWindow.value;
         caretPosition.start = editWindow.selectionStart;
         caretPosition.end = editWindow.selectionEnd;
         console.debug("caretPosition: ", caretPosition);
+        background.replaceChildren([
+            document.createTextNode(editText.substring(0, caretPosition.end)),
+            fakeCaret,
+            document.createTextNode(editText.substring(caretPosition.end))
+        ])
     });
 }, false);
 console.log("stopgap.js loaded");
