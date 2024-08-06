5ad1b77a3dfca1417cd16c46474242a7af5288b7
John Comeau <jc@unternet.net>
2024-08-06 06:49:32 -0700

    working better now, finally. but remote not seeing keyhits on local

--- stopgap/stopgap.py
+++ stopgap/stopgap.py
@@ -159,11 +159,11 @@ def handler(connection):
                     # serial numbers must be nonzero, so increment first.
                     # same key/serial number gets sent to all clients.
                     serial += 1
-                    message.pop('echo')
+                    echo = message.pop('echo')
                     message.update({'serial': serial})
                     serialized = pack(message)
                     for client in CLIENTS:
-                        if client is connection and not message['echo']:
+                        if client is connection and not echo:
                             logging.debug('not echoing %s back to sender %s',
                                           message['key'], client)
                         else:
